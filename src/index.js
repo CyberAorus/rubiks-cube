b1b9c7840e458ae7a88372b4d7db74256bda909a
IVAYLO MARKOV <ivo.markoff@gmail.com>
2022-06-11 13:42:38 +0100

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 const app = express();
 const port = 5000;
 
+app.use('/static', express.static('public'));
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+    
+
 app.get('/', (req, res) => {
-    res.send('App connected!');
+    res.render('index');
 });
 
 app.listen(port, () => {
