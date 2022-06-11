859db7993cfcf73818cb035c4c79f937aa27a970
IVAYLO MARKOV <ivo.markoff@gmail.com>
2022-06-11 16:25:33 +0100

    Add cube services

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,28 +1,26 @@
 const router = require('express').Router();
-const fs = require('fs/promises');
-const cubes = require('../db.json');
-const path = require('path');
+const cubeService = require('../services/cubeService');
 
 router.get('/create', (req, res) => {
     res.render('create');
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const cube = req.body;
     //TODO cube vallidation
     if (cube.name.length < 3) {
         res.status(400).send('Cube name should be at least 3 symbols long');
         return;
     }
+    //TODO save cube to db
+    try {
+        await cubeService.save(cube);
+        res.redirect('/');
 
-    cubes.push(cube);
-    fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {urlenconded: 'utf8'})
-        .then(() => {
-            res.redirect('/');
-        })
-        .catch(err => {
-            res.status(500).send(err);
-        })
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
 });
 
 module.exports = router;
