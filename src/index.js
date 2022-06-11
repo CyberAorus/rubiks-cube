fe6d02ae0b0ed16a36ad2a96a9aa98ea7d8adf27
IVAYLO MARKOV <ivo.markoff@gmail.com>
2022-06-11 14:15:54 +0100

    Add home controller and router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const router = require('./routes');
+
 const app = express();
 const port = 5000;
+
 
 app.use('/static', express.static('public'));
 
@@ -11,19 +14,7 @@ app.engine('hbs', hbs.engine({
 
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
-    
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/create', (req, res) => {
-    res.render('create');
-});
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
