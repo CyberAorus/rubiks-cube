baaaca29d2d0312c6332828431c4c5d156d22256
IVAYLO MARKOV <ivo.markoff@gmail.com>
2022-06-18 11:08:53 +0100

    User register

---
+++ src/services/authService.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+exports.register = async (userData) => {
+    const { username, password, repeatPassword } = userData;
+    //User.create(userData);
+    // return form validation message
+    if (password !== repeatPassword) {
+        throw new Error('Passwords do not match');
+    }
+
+    let hashedPassword = await bcrypt.hash(password, saltRounds);
+    let createdUser = await User.create({
+        username,
+        password: hashedPassword
+    });
+
+    return createdUser;
+}
